b8876cee028511805d862a37fc2a71d0c7e92776
lev0607 <lev36rus@gmail.com>
2020-04-09 16:04:53 +0300

    add migration domain_checks

---
+++ database/migrations/2020_04_09_124443_create_domain_checks_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDomainChecksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('domain_checks', function (Blueprint $table) {
+            $table->id();
+            $table->foreign('domain_id')->references('id')->on('domains');
+            $table->integer('status_code');
+            $table->text('h1')->nullable();
+            $table->text('keywords')->nullable();
+            $table->text('description')->nullable();
+            $table->timestamps();
+        });
+    }
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('domain_checks');
+    }
+}
